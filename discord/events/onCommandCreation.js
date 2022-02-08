c430d92ab2d64e4d3c9e16060a8bb611cbcbc70d
Cryptizism <cryptizism@protonmail.com>
2022-02-08 22:38:21 +0000

    Updated database stuff ig

--- discord/events/onCommandCreation.js
+++ discord/events/onCommandCreation.js
@@ -3,22 +3,34 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 
     stats : function(message, client) {
+        var games = [];
+
         var guild = client.guilds.cache.get("788094168618106880");
+
+        var guildMemebers = guild.members.cache;
         
-        var onlineCount = guild.members.cache.filter(m => m.presence?.status === 'online').size;
+        var onlineCount = guildMemebers.filter(m => m.presence?.status === 'online').size;
 
-        var idleCount = guild.members.cache.filter(m => m.presence?.status === 'idle').size;
+        var idleCount = guildMemebers.filter(m => m.presence?.status === 'idle').size;
 
-        var dndCount = guild.members.cache.filter(m => m.presence?.status === 'dnd').size;
+        var dndCount = guildMemebers.filter(m => m.presence?.status === 'dnd').size;
 
-        var totalCount = guild.members.cache.size;
+        guildMemebers.forEach(member => {
+            console.log(member.presence?.activities[0].name);
+
+            if (member.presence?.activities || member.presence?.activities.length > 0) {
+                games.push(member.presence.activities[0].name);
+            }
+        });
+
+        var totalCount = guildMemebers.size;
 
         var offlineCount = totalCount - (onlineCount + idleCount + dndCount);
 
         var embed = new MessageEmbed()
             .setTitle('Server Stats')
             .setColor('#0099ff')
-            .setDescription(`**Online:** ${onlineCount}\n**Offline:** ${offlineCount}\n**Idle:** ${idleCount}\n**DND:** ${dndCount}\n**Total:** ${totalCount}`);
+            .setDescription(`**Online:** ${onlineCount}\n**Offline:** ${offlineCount}\n**Idle:** ${idleCount}\n**DND:** ${dndCount}\n**Total:** ${totalCount}\n**Games:** ${games.join(', ')}`);
 
         return embed;
     }
